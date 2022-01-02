b45474d8b6b16c4e4d17ff956ba1ae9a58a6a158
SoliSowmya <sowmyareddys213@gmail.com>
2022-01-02 20:23:32 +0530

    commit

--- urlshortner/urls.py
+++ urlshortner/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.contrib.auth import logout
-from django.urls import path
+from django.urls import path,
 from authentication.views import login,signup,logout
 from urlhandler.views import dashboard,generate,home,deleteurl
+
+
 urlpatterns = [
     
     path('admin/', admin.site.urls),
