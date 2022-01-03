f43401ad4bdd3d3df835c74b39c4a81d869ecc8f
SoliSowmya <sowmyareddys213@gmail.com>
2022-01-03 13:06:15 +0530

    commit

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wb=6&a0^fqg7ds*0cwgsa$!+f$#bq92a^(=$2ov7v9(xysvc9='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','shortlyyyyy.herokuapp.com']
 
